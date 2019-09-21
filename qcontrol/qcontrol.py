d4b43f8c0a88bc7a9450cd58cae156e0318b7a6b
Kurt Jacobson <kcjengr@gmail.com>
2019-09-21 15:39:22 -0400

    add notifications label at top of screen

--- qcontrol/qcontrol.py
+++ qcontrol/qcontrol.py
@@ -1,5 +1,7 @@
 from qtpyvcp.widgets.form_widgets.main_window import VCPMainWindow
 
+# from qtpyvcp.actions import action
+
 # Setup logging
 from qtpyvcp.utilities import logger
 LOG = logger.getLogger('qtpyvcp.' + __name__)
@@ -10,3 +12,12 @@ class QControlMainWindow(VCPMainWindow):
         super(QControlMainWindow, self).__init__(*args, **kwargs)
 
         self.setWindowTitle("LinuxCNC - QControl")
+
+    # @action('window.maximize')
+    # def maximize(self):
+    #     self.showMaximized()
+
+    def on_notificationButton_pressed(self):
+        self.notificationButton.setText("")
+        self.notificationButton.setStyleSheet("background-color: ")
+
